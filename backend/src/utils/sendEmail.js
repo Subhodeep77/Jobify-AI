c16bcfb32ab8fab90b213fde904df82772ea79d5
Subhodeep77 <subhodeepparamanik22413@gmail.com>
2026-07-23 00:19:34 +0530

    backend

--- backend/src/utils/sendEmail.js
+++ backend/src/utils/sendEmail.js
@@ -1,34 +1,14 @@
-import nodemailer from "nodemailer";
+import { Resend } from "resend";
 
-const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-  connectionTimeout: 10000,
-  greetingTimeout: 10000,
-  socketTimeout: 10000,
-  logger: true,
-  debug: true,
-});
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendEmail = async ({ to, subject, text }) => {
-  console.log("SMTP: verifying...");
-  await transporter.verify();
-  console.log("SMTP: verified");
-
-  console.log("SMTP: sending...");
-  const info = await transporter.sendMail({
-    from: `"Support" <${process.env.EMAIL_USER}>`,
+  await resend.emails.send({
+    from: "onboarding@resend.dev",
     to,
     subject,
     text,
   });
-
-  console.log("SMTP: sent", info.messageId);
 };
 
 export default sendEmail;
